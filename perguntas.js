e1b2715e9a514be4f07e1be9d9b476ab55c91535
Eduardo41t <142507224+Eduardo41t@users.noreply.github.com>
2024-12-09 23:17:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    ' Como funciona a Primeira Lei de Newton, também conhecida como Lei da Inércia?',
+    ' Primeira Lei de Newton, ou Lei da Inércia, afirma que um objeto em repouso permanecerá em repouso e um objeto em movimento continuará em movimento com velocidade constante em linha reta, a menos que uma força externa seja aplicada sobre ele. Em outras palavras, a tendência natural dos objetos é manter seu estado de movimento, seja ele de repouso ou de movimento uniforme, até que uma força atue sobre eles para mudar esse estado'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o país com a maior população do mundo?',
+    ' A China é o país com a maior população do mundo, com mais de 1,4 bilhão de habitantes. No entanto, a Índia está muito próxima e deve ultrapassar a China nos próximos anos, de acordo com as estimativas demográficas'
+)
+
+criaCartao(
+    'jogo',
+    'Qual é o objetivo principal do jogo "Minecraft"?',
+    'O objetivo principal de "Minecraft" é explorar, minerar e construir no mundo virtual gerado proceduralmente. Os jogadores podem coletar recursos, construir estruturas, criar ferramentas e sobreviver em um ambiente onde enfrentam monstros e desafios. O jogo oferece modos como o "Sobrevivência", onde o jogador deve gerenciar saúde e fome, e o "Criativo", onde não há limitações de recursos e o foco é na construção livre'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Gostar em ingles é Like (laike)'
+)
